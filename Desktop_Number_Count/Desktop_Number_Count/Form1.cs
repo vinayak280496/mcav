b5c9abc09635ca4c0f6297db4acd1bc67e80d08f
Shubham Sajannavar <shub2495@gmail.com>
2019-09-12 14:32:04 +0530

    upload via GitBash

---
+++ Desktop_Number_Count/Desktop_Number_Count/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Desktop_Number_Count
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                progressBar1.Value = i;             
+                Thread.Sleep(100);
+                if(i==100){
+                    
+                }
+
+            }      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are about to logout.");
+            Application.Exit();
+        }
+    }
+}
