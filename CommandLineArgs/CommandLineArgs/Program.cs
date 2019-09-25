4e35834beb9211e2dccbe42ed3bb94edb05617df
Shubham Sajannavar <shub2495@gmail.com>
2019-09-25 22:27:36 +0530

    Changed made

---
+++ CommandLineArgs/CommandLineArgs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLineArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "file:///C:/Users/CA172007/AppData/Local/Temporary Projects/ConsoleApplication1/bin/Debug/ConsoleApplication1.EXE";
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("This Program is developed by Shubham Sajannavar");
+            Console.WriteLine("Roll No : CA172007, Rani Channamma University, Belgavi");
+            Console.WriteLine("---------------------------------------------------------\n");
+            try
+            {
+                if (args.Length >= 2)
+                {
+                    int num1 = Int32.Parse(args[0]);
+                    int num2 = Int32.Parse(args[1]);
+                    int sum = num1 + num2;
+
+                    Console.WriteLine("CommandLine Args : " + num1 + " and " + num2);
+                    Console.WriteLine("\nAddition of Command Line Args : {0}", sum);
+                }
+                else
+                {
+                    Console.WriteLine("No Command Line Args Passed.");
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Invalid Args Passwd");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Enumerations/Enumerations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Enumerations
+{
+    class Program
+    {
+        enum CollegeDays
+        {
+            MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+        }   
+        static void Main(string[] args)
+        {
+            Console.Title = "file:///C:/Users/CA172007/AppData/Local/Temporary Projects/ConsoleApplication1/bin/Debug/ConsoleApplication1.EXE";
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("This Program is developed by Shubham Sajannavar");
+            Console.WriteLine("Roll No : CA172007, Rani Channamma University, Belgavi");
+            Console.WriteLine("---------------------------------------------------------\n");
+
+            foreach (var day in Enum.GetValues(typeof(CollegeDays)))
+            {
+                Console.WriteLine(">> {0} : {1}", day, (int)day);
+            }
+            Console.ReadKey();
+        }
+    }
+}
