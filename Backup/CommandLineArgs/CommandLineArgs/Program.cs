00026a4686e0f29884a60f25f3f9c8eb8c64cfdf
Shubham Sajannavar <shub2495@gmail.com>
2019-09-29 19:17:07 +0530

    Backup added

---
+++ Backup/CommandLineArgs/CommandLineArgs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CommandLineArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                int num1 = Convert.ToInt32(args[0]);
+                int num2 = Convert.ToInt32(args[1]);
+
+                int sum = num1 + num2;
+
+                Console.WriteLine("CommandLine Args are : " + num1 + " and " + num2 + ".");
+                Console.WriteLine("Addition of "+num1+" and "+num2+" : "+sum);
+            }
+            else {
+                Console.WriteLine("More then two ares are passed.");
+            }
+            Console.ReadKey();
+
+            
+        }
+
+    }
+}
---
+++ Backup/UpperToLower/UpperToLower/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UpperToLower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int i, len;
+            char[] arr;
+            char ch;
+
+            Console.WriteLine("Enter a string");
+            str = Console.ReadLine();
+            len = str.Length;
+
+            arr = str.ToCharArray(0,len);
+            Console.WriteLine("After Conversion");
+
+            for (i = 0; i < len; i++) {
+                ch=arr[i];
+                if (Char.IsLower(ch))
+                {
+                    Console.Write(Char.ToUpper(ch));
+                }
+                else {
+                    Console.Write(Char.ToLower(ch));
+                }
+            
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
+
